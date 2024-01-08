a07546eb7b98c0ff3b46b34b929035b12afe4567
Shamimreza82 <shamimrezaone@gmail.com>
2024-01-08 18:23:40 +0600

    update code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // const {createStore} = require("redux")
 
+const { createStore } = require("redux")
+
 
 // const increment = "INCREMENT"; 
 // const Decrement = "DECREMENT"; 
@@ -66,10 +68,91 @@
 
 // store.dispatch(IncrementCounter())
 
-const GET_PRODUCTS = "GET_PRODUCTS"
-const ADD_PRODUCTS = "ADD_PRODUCTS"
+// const GET_PRODUCTS = "GET_PRODUCTS"
+// const ADD_PRODUCTS = "ADD_PRODUCTS"
 
-const initialProductState = {
-    prodects: ["sugar", "salt"], 
-    numberOfProducts: 2
+// const initialProductState = {
+//     prodects: ["sugar", "salt"], 
+//     numberOfProducts: 2
+// }
+
+
+
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST"
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS"
+const GET_TODOS_FAILED = "GET_TOTDOS_FAILED"
+
+//// https://jsonplaceholder.typicode.com/todos
+
+
+/// state 
+const initialTodosState = {
+    todos: [], 
+    isloading: false, 
+    error: null
 }
+
+
+/// action 
+const getTodosRequest = () => {
+    return {
+        type: GET_TODOS_REQUEST
+    }
+}
+const getTodosFailed = (error) => {
+    return {
+        type: GET_TODOS_FAILED, 
+        payload: error
+}
+}
+
+const getTodosSuccess = (todos) => {
+    return {
+        type: GET_TODOS_SUCCESS, 
+        payload: todos
+}
+}
+
+
+// reducers 
+
+const todosReducer = (state = initialTodosState, action) => {
+    switch (action.type) {
+        case GET_TODOS_REQUEST:
+            return{
+                ...state, 
+                isLoading: true, 
+            }
+
+        case GET_TODOS_SUCCESS:
+            return{
+                ...state, 
+                isLoading: false, 
+                todos: action.payload 
+            }
+
+        case GET_TODOS_FAILED:
+            return{
+                ...state, 
+                isLoading: false, 
+                error: action.payload
+            }
+        default:
+           return state;
+    }
+}
+
+/// aysen Action   
+ const fetchData = () => {
+
+ }
+
+
+//store 
+
+const store = createStore(todosReducer); 
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch(fetchData())
